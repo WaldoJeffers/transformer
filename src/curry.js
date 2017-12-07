5e9a32a66f249d149e4055997836bbc393b7148b
Achille Urbain <achille.urbain@gmail.com>
2017-12-08 00:49:46 +0100

    Remove curry function (not used anymore)

--- src/curry.js
+++
@@ -1,8 +0,0 @@
-const curryN = (N, fn) => (...args) =>
-  args.length === N
-    ? fn(...args)
-    : curryN(N - args.length, (...args2) => fn(...args, ...args2))
-
-const curry = fn => curryN(fn.length, fn)
-
-export default curry
