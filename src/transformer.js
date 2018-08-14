43f080db15983e3edf1f8957488e63a2472409eb
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2018-08-14 23:48:40 +0200

    Update index.js

--- src/transformer.js
+++
@@ -1,7 +0,0 @@
-import identity from './identity'
-import extend from './extend'
-import map from './map'
-import mapFrom from './mapFrom'
-import transform from './transform'
-
-export { identity, extend, map, mapFrom, transform }
