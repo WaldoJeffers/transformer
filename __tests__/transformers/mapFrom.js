5c371eff8c4a32dc6b3469eb0a80c4294e43da81
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-01-04 12:37:58 +0100

    fix: mapFrom should use an identity mapper if no mapper is provided

--- __tests__/transformers/mapFrom.js
+++ __tests__/transformers/mapFrom.js
@@ -1,71 +1,77 @@
-import mapFrom from '../../src/transformers/mapFrom'
-import assoc from '../../src/utils/assoc'
-import { isTransformer } from '../../src/transformers/transformer'
+import mapFrom from "../../src/transformers/mapFrom"
+import assoc from "../../src/utils/assoc"
+import { isTransformer } from "../../src/transformers/transformer"
 
 const input = {
-  age: '27',
-  firstname: 'James',
-  lastname: 'M',
-  occupation: 'Drummer',
-  drumsticks: 2,
+  age: "27",
+  firstname: "James",
+  lastname: "M",
+  occupation: "Drummer",
+  drumsticks: 2
 }
 
-describe('mapFrom', () => {
-  it('should return a transformer', () => {
+describe("mapFrom", () => {
+  it("should return a transformer", () => {
     expect(isTransformer(mapFrom())).toBe(true)
   })
 
-  it('should pick the property and run the mapper on its value', () => {
+  it("should pick the property and run the mapper on its value", () => {
     expect(
-      mapFrom('drumsticks', x => 2 * x)(assoc('DrumSticks'))({}, input)
+      mapFrom("drumsticks", x => 2 * x)(assoc("DrumSticks"))({}, input)
     ).toEqual({
-      DrumSticks: 4,
+      DrumSticks: 4
     })
   })
 
-  it('should pass the picked properties to a 1-arity mapper and run mapper on their value', () => {
+  it("should simply pick the property if no mapper is passed", () => {
+    expect(mapFrom("firstname")(assoc("firstname"))({}, input)).toEqual({
+      firstname: "James"
+    })
+  })
+
+  it("should pass the picked properties to a 1-arity mapper and run mapper on their value", () => {
     expect(
       mapFrom(
-        ['firstname', 'lastname'],
+        ["firstname", "lastname"],
         ([firstname, lastname]) => `${firstname} ${lastname}`
-      )(assoc('fullname'))({}, input)
+      )(assoc("fullname"))({}, input)
     ).toEqual({
-      fullname: 'James M',
+      fullname: "James M"
     })
   })
 
-  it('should not pick an undefined property', () => {
-    expect(mapFrom('middlename')(assoc('middlename'))({}, input)).toEqual({})
+  it("should not pick an undefined property", () => {
+    expect(mapFrom("middlename")(assoc("middlename"))({}, input)).toEqual({})
   })
 
-  it('should not run the mapper on an undefined property', () => {
+  it("should not run the mapper on an undefined property", () => {
     expect(
-      mapFrom('middlename', x => x.toUpperCase())(assoc('middlename'))(
+      mapFrom("middlename", x => x.toUpperCase())(assoc("middlename"))(
         {},
         input
       )
     )
   })
 
-  it('should throw if no mapper is provided', () => {
-    expect(mapFrom('firstname')(assoc('fullname'))).toThrow()
-    expect(mapFrom(['firstname', 'lastname'])(assoc('fullname'))).toThrow()
+  it("should throw if no mapper is provided", () => {
+    expect(mapFrom("firstname")(assoc("fullname"))).toThrow()
+    expect(mapFrom(["firstname", "lastname"])(assoc("fullname"))).toThrow()
   })
 
-  it('should not work on an array of props containing an unkown prop', () => {
+  it("should not work on an array of props containing an unkown prop", () => {
     expect(
-      mapFrom(['firstname', 'middlename'])(assoc('fullname'))({}, input)
+      mapFrom(["firstname", "middlename"])(assoc("fullname"))({}, input)
     ).toEqual({})
   })
 
-  it.skip('on an array of props with a mapper accepting more than 1 argument', () => {
+  it.skip("on an array of props with a mapper accepting more than 1 argument", () => {
     expect(
       mapFrom(
-        ['firstname', 'lastname'],
+        ["firstname", "lastname"],
         (firstname, lastname) => `${firstname} ${lastname}`
-      )(assoc('fullname'))({}, input)
+      )(assoc("fullname"))({}, input)
     ).toEqual({
-      fullname: 'James M',
+      fullname: "James M"
     })
   })
 })
--- src/transformers/mapFrom.js
+++ src/transformers/mapFrom.js
@@ -1,15 +1,16 @@
-import filter from './filter'
-import map from './map'
-import compose from '../utils/compose'
-import has from '../utils/has'
-import get from '../utils/get'
-import pick from '../utils/pick'
+import filter from "./filter"
+import map from "./map"
+import compose from "../utils/compose"
+import has from "../utils/has"
+import get from "../utils/get"
+import pick from "../utils/pick"
+import identity from "../utils/identity"
 
-const mapFrom = (keys, mapper) =>
+const mapFrom = (keys, mapper = identity) =>
   compose(
     filter(has(keys)),
     map(Array.isArray(keys) ? pick(keys) : get(keys)),
-    map(mapper),
+    map(mapper)
   )
 
 export default mapFrom
